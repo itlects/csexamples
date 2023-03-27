c67a5f3068a9ff64837eac1370329032acf45d2d
unknown <itlects@gmail.com>
2023-03-27 14:44:19 +0900

    c#demo basic, classes: properties, inheritance, polymorphism

---
+++ 00.console/Classes/Inheritance/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inheritance demo!");
+            Console.WriteLine();
+            
+            Console.WriteLine("1) inheritanceCarDemo()!");
+            inheritanceCarDemo();
+            Console.WriteLine();
+
+            Console.WriteLine("2) inheritanceAnimalSoundDemo()!");
+            inheritanceAnimalSoundDemo();
+            Console.WriteLine();
+        }
+
+        //------------------------------------
+        // inheritanceCarDemo()
+        private static void inheritanceAnimalSoundDemo()
+        {
+            Animal myAnimal = new Animal();  // Create a Animal object
+            Animal myPig = new Pig();  // Create a Pig object
+            Animal myDog = new Dog();  // Create a Dog object
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
+        }
+
+        private static void inheritanceCarDemo()
+        {
+            // Create a myCar object
+            Car myCar = new Car();
+
+            // Call the honk() method (From the Vehicle class) on the myCar object
+            myCar.honk();
+
+            // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
+        }
+
+        class Vehicle  // base class (parent) 
+        {
+            public string brand = "Ford";  // Vehicle field
+            public void honk()             // Vehicle method 
+            {
+                Console.WriteLine("Tuut, tuut!");
+            }
+        }
+
+        //------------------------------------
+        // anaimalSoundDemo()
+        class Car : Vehicle  // derived class (child)
+        {
+            public string modelName = "Mustang";  // Car field
+        }
+
+
+        class Animal  // Base class (parent) 
+        {
+            public void animalSound()
+            {
+                Console.WriteLine("The animal makes a sound");
+            }
+        }
+
+        class Pig : Animal  // Derived class (child) 
+        {
+            public void animalSound()
+            {
+                Console.WriteLine("The pig says: wee wee");
+            }
+        }
+
+        class Dog : Animal  // Derived class (child) 
+        {
+            public void animalSound()
+            {
+                Console.WriteLine("The dog says: bow wow");
+            }
+        }
+    }
+}
---
+++ 00.console/Classes/Properties/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Person
+    {
+        private string name; // field
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
